e7588fd2313534c9029f1b01595a7d880037316e
TsaiHC <140023207+Hao-Che-Tsai@users.noreply.github.com>
2023-07-19 16:07:38 -0700

    Delete VLSI_HW2-3 directory

--- VLSI_HW2-3/report_files/script.sh
+++
@@ -1,4 +0,0 @@
-vlog arbiter.sv -work rtl_work
-vsim rtl_work.arbiter -c  -voptargs=+acc -t ns -do arbiter.do
-design_vision-xg -no_gui -f synthesis.do
-
--- VLSI_HW2-3/scripts/synthesis.do
+++
@@ -1,5 +0,0 @@
-read_sverilog arbiter.sv
-compile
-write -hierarchy -format verilog -output arbiter.gate.v
-report_hierarchy
-exit
